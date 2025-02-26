cfdb844b962962614035012717375899b0632fb6
AbhishekA1 <abhishekanandpawar1@gmail.com>
2025-02-27 00:30:59 +0530

    Todo with task and status

---
+++ Todo/src/components/Todo.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import "../css/Todo.css"
+
+export function Todo() {
+    let [data, setData] = useState([])
+    let [input, setInput] = useState("")
+    let [select,setSelect]=useState("default")
+    
+    function handleInput(event) {
+        setInput(event.target.value)
+       console.log(event.target.value)
+    }
+    function handleSelect(event) {
+      setSelect(event.target.value);
+    }
+
+    function handleAdd() {
+        setData([...data, { task: input, status: select }])
+        setInput("")
+    }
+    return (
+        <>
+            <h1>Todo</h1>
+            <input type="text" placeholder="Enter task" value={input} onChange={handleInput}/>
+            <select onChange={handleSelect}>
+                <option value="default">Default</option>
+                <option value="pending">Pending</option>
+                <option value="completed">Completed</option>
+            </select>
+            <button onClick={handleAdd}>Add task</button>
+
+            <div className="parent">
+                {
+                    data.map((ele,id) => {
+                        return (
+                          <div key={id} className="child">
+                                <h1>Task: {ele.task}</h1>
+                                <h2>Status: {ele.status }</h2>
+                          </div>
+                        );
+                    })
+                }
+            </div>
+        </>
+    )
+}
